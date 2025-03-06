f08d6d62a97155d6885760960ef771f922d4f75d
Carter <152464866+caltman05@users.noreply.github.com>
2025-03-06 10:19:41 -0500

    Update

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
+    
+    var secondsRemaining = 60
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining=eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0,target:self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+            
+    @objc func updateTimer(){
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining-=1
+        }
+    }
+    
+    
+    
+        }
+            
 
-}
